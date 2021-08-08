536093f4fcc513559e62a62bb9fed8460494c976
Thiago Oliveira <pintowar@gmail.com>
2021-08-08 11:42:29 -0300

    adding sonarqube plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("sudoscan.kotlin-base")
     id("net.researchgate.release")
+    id("org.sonarqube")
 }
 
 allprojects {
@@ -48,6 +49,23 @@ tasks {
 
             logger.quiet("JAR generated at $rootDir/build/. It combines the server and client projects.")
         }
+    }
+}
+
+sonarqube {
+    properties {
+        val jacocoReportPath = "${project.buildDir.absolutePath}/reports/jacoco/codeCoverageReport"
+        val sonarToken = project.findProperty("sonar.token")?.toString() ?: System.getenv("SONAR_TOKEN")
+        property("sonar.sourceEncoding", "UTF-8")
+        property("sonar.organization", "pintowar")
+        property("sonar.projectName", "sudoscan")
+        property("sonar.projectKey", "pintowar_sudoscan")
+        property("sonar.projectVersion", project.version.toString())
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.login", sonarToken)
+        property("sonar.verbose", true)
+        property("sonar.github.repository", "pintowar/sudoscan")
+        property("sonar.coverage.jacoco.xmlReportPaths", "$jacocoReportPath/codeCoverageReport.xml")
     }
 }
 
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.5.0")
     implementation("org.bytedeco.gradle-javacpp-platform:org.bytedeco.gradle-javacpp-platform.gradle.plugin:1.5.6")
     implementation("gradle.plugin.com.github.jengelman.gradle.plugins:shadow:7.0.0")
+    implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:3.3")
     implementation("io.kotest:kotest-gradle-plugin:0.3.8")
     implementation("net.researchgate:gradle-release:2.8.1")
 }
