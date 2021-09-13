41c1d855e9fa7ba9e4b9c34858cd0e24f68cc57d
Thiago Oliveira <pintowar@gmail.com>
2021-09-13 10:56:45 -0300

    adjusting webcam picture component (UI)

--- sudoscan-webclient/src/App.tsx
+++ sudoscan-webclient/src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import {FaBeer} from 'react-icons/fa'
+import { WebCamPicture } from './components/WebCamPicture'
 
 function App() {
   return (
-    <div className="App">
-      <h1><FaBeer/> Hello Components</h1>
+    <div>
+      <nav className="bg-gray-800">
+        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="relative flex items-center justify-between h-16">
+            
+            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex-shrink-0 flex items-center">
+                <img className="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow"/>
+                <img className="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow"/>
+              </div>
+              <div className="hidden sm:block sm:ml-6">
+                <div className="flex space-x-4">
+                  <a href="#" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Picture</a>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </nav>
+      <WebCamPicture/>
     </div>
   );
 }
---
+++ sudoscan-webclient/src/components/WebCamPicture.tsx
@@ -0,0 +1,55 @@
+import { useRef, useCallback, useState } from "react";
+import { FaCamera, FaCalculator, FaRegTrashAlt } from 'react-icons/fa';
+import Webcam from "react-webcam";
+
+export const WebCamPicture = () => {
+    const noImage = "./no-image.png"
+    const [imgSource, setImgSource] = useState(noImage);
+    const [alert, setAlert] = useState(false);
+    const webcamRef = useRef<Webcam>(null);
+    const [imgWidth, imgHeight] = [640, 480]
+    const imgStyle = {width: `${imgWidth}px`, height: `${imgHeight}px`}
+
+    const capture = useCallback(() => {
+        const screenshot = webcamRef.current?.getScreenshot() || ""
+        setImgSource(screenshot)
+    }, [webcamRef])
+
+    const clean = useCallback(() => {
+        setImgSource(noImage)
+    }, [webcamRef])
+
+    return (
+        <>
+            { alert &&
+                <div role="alert">
+                    <div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">Danger</div>
+                    <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+                        <p>Something not ideal might be happening.</p>
+                    </div>
+                </div>    
+            }
+
+            <div className="flex justify-center mt-5 space-x-5" >
+                <Webcam audio={false} ref={webcamRef} width={imgWidth} height={imgHeight} screenshotFormat="image/jpeg"/>
+                <img src={imgSource} className="object-scale-down" style={imgStyle}/>
+            </div>
+            
+            <div className="flex justify-center space-x-2">
+                <button onClick={capture} className="px-4 py-3 my-5 bg-blue-500 hover:bg-blue-700 rounded-full text-white font-bold flex">
+                    <FaCamera className="h-6 w-6"/>
+                    <span className="ml-2">Capture</span>
+                </button>
+                <button className="px-4 py-3 my-5 bg-blue-500 hover:bg-blue-700 rounded-full text-white font-bold flex">
+                    <FaCalculator className="h-6 w-6"/>
+                    <span className="ml-2">Solve</span>
+                </button>
+                <button onClick={clean} className="px-4 py-3 my-5 bg-blue-500 hover:bg-blue-700 rounded-full text-white font-bold flex">
+                    <FaRegTrashAlt className="h-6 w-6"/>
+                    <span className="ml-2">Clean</span>
+                </button>
+            </div>
+            
+        </>
+    );
+};
