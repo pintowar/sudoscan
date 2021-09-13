c6b4ca978ab37f4c78986fe97db272c0da3b9052
Thiago Oliveira <pintowar@gmail.com>
2021-09-12 21:31:42 -0300

    adding web client module (React + TypeScript)

---
+++ sudoscan-webclient/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {FaBeer} from 'react-icons/fa'
+
+function App() {
+  return (
+    <div className="App">
+      <h1><FaBeer/> Hello Components</h1>
+    </div>
+  );
+}
+
+export default App;
