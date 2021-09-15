f230ada88e7a28ec193b0555c29fddabd6658d2c
Thiago Oliveira <pintowar@gmail.com>
2021-09-15 01:20:31 -0300

    adjusting webclient gradle config

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     implementation("io.micronaut.gradle:micronaut-gradle-plugin:2.0.4")
     implementation("org.bytedeco.gradle-javacpp-platform:org.bytedeco.gradle-javacpp-platform.gradle.plugin:1.5.6")
     implementation("gradle.plugin.com.github.jengelman.gradle.plugins:shadow:7.0.0")
+    implementation("com.github.node-gradle:gradle-node-plugin:3.1.1")
     implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:3.3")
     implementation("io.kotest:kotest-gradle-plugin:0.3.8")
     implementation("net.researchgate:gradle-release:2.8.1")
