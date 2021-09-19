bbd31a23753b716590847e59540ce21134800fb8
Thiago Oliveira <pintowar@gmail.com>
2021-09-19 18:57:00 -0300

    adding about page

--- sudoscan-webclient/src/pages/About.tsx
+++ sudoscan-webclient/src/pages/About.tsx
@@ -7,13 +7,13 @@ export const About = () => {
         
         <div className="container pt-16 mx-auto items-center">
             <div className="flex flex-col items-center justify-center">
-                <h1 className="text-6xl leading-normal">Sudoscan Web</h1>
+                <h1 className="text-5xl leading-normal">Sudoscan Web</h1>
                 <p className="leading-normal mb-12">
                         This Project contains a series of components. Here ia a list of the main components used by the project.
                 </p>
 
                 <h2 className="text-3xl leading-normal">Solvers</h2>
-                <ul>
+                <ul className="space-y-2 mt-4">
                     <li>
                         <a href="https://choco-solver.org/" target="_blank" rel="noreferrer">
                             <img src="https://choco-solver.org/images/logo-choco-small.webp" 
@@ -23,7 +23,7 @@ export const About = () => {
                 </ul>
         
                 <h2 className="text-3xl leading-normal">Recognizers</h2>
-                <ul>
+                <ul className="space-y-2 mt-4">
                     <li>
                         <a href="https://deeplearning4j.org/" target="_blank" rel="noreferrer">
                             <img src="https://deeplearning4j.org/images/logo.png" 
@@ -39,7 +39,7 @@ export const About = () => {
                 </ul>
 
                 <h2 className="text-3xl leading-normal">Server Side</h2>
-                <ul>
+                <ul className="space-y-2 mt-4">
                     <li>
                         <a href="https://micronaut.io/" target="_blank" rel="noreferrer">
                             <img src="https://objectcomputing.com/files/2716/2256/3799/micronaut_stacked_black.png" 
@@ -49,7 +49,7 @@ export const About = () => {
                 </ul>
 
                 <h2 className="text-3xl leading-normal">Client Side</h2>
-                <ul>
+                <ul className="space-y-2 mt-4">
                     <li>
                         <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
                             <img src="https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg" 
