7e7d9e2b013c2ae34e07a69e90488324beb0c022
Thiago Oliveira <pintowar@gmail.com>
2023-03-03 13:19:41 -0300

    adding sonatype repo as publishing repo

--- buildSrc/src/main/kotlin/sudoscan.kotlin-publish.gradle.kts
+++ buildSrc/src/main/kotlin/sudoscan.kotlin-publish.gradle.kts
@@ -14,6 +14,17 @@ publishing {
                 password = project.findProperty("gpr.pass")?.toString() ?: System.getenv("GITHUB_TOKEN")
             }
         }
+
+        maven {
+            name = "Sonatype"
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            setUrl(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            credentials {
+                username = project.findProperty("ossrh.user")?.toString() ?: System.getenv("SONATYPE_USER")
+                password = project.findProperty("ossrh.pass")?.toString() ?: System.getenv("SONATYPE_PASS")
+            }
+        }
     }
     publications {
         register<MavenPublication>("gpr") {
