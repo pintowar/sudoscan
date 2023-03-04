0aec7563962b5c152e036ed901f3898d32422d16
Thiago Oliveira <pintowar@gmail.com>
2023-03-04 17:01:18 -0300

    upgrading micronaut version

--- buildSrc/src/main/kotlin/sudoscan.kotlin-base.gradle.kts
+++ buildSrc/src/main/kotlin/sudoscan.kotlin-base.gradle.kts
@@ -35,6 +35,9 @@ dependencies {
 }
 
 java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
 }
